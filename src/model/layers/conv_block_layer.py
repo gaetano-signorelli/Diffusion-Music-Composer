4c52b52ac5134b816e4bf64d471cd991b21c605e
gaetano-signorelli <gae797@yahoo.it>
2023-01-24 16:03:17 +0100

    Causal convolution implemented

--- src/model/layers/conv_block_layer.py
+++ src/model/layers/conv_block_layer.py
@@ -15,13 +15,13 @@ class ConvolutionalBlockLayer(layers.Layer):
             mid_channels = out_channels
 
         self.conv_layer_1 = layers.Conv2D(mid_channels,
-                                        kernel_size=kernel_size,
-                                        padding="same",
+                                        kernel_size=(1,kernel_size),
+                                        padding="valid",
                                         use_bias=False)
 
         self.conv_layer_2 = layers.Conv2D(out_channels,
-                                        kernel_size=kernel_size,
-                                        padding="same",
+                                        kernel_size=(1,kernel_size),
+                                        padding="valid",
                                         use_bias=False)
 
         self.group_norm_layer_1 = layers.LayerNormalization()
@@ -30,16 +30,23 @@ class ConvolutionalBlockLayer(layers.Layer):
         self.squeeze_excitation_layer_1 = SqueezeAndExcitationLayer(mid_channels)
         self.squeeze_excitation_layer_2 = SqueezeAndExcitationLayer(out_channels)
 
+        w_padding = (kernel_size - 1)
+        self.padding_tensor_width = tf.constant([ [0,0], [0,0], [w_padding,0], [0,0]])
+        #Width padding is only on the left side, making this a Causal convolution, like for WaveNet
+
     @tf.function
     def call(self, x):
 
-        conv_x = self.conv_layer_1(x)
+        padded_x = tf.pad(x, self.padding_tensor_width, "CONSTANT")
+
+        conv_x = self.conv_layer_1(padded_x)
         conv_x = self.group_norm_layer_1(conv_x)
         conv_x = activations.gelu(conv_x)
-
         conv_x = self.squeeze_excitation_layer_1(conv_x)
 
-        conv_x = self.conv_layer_2(conv_x)
+        padded_x = tf.pad(conv_x, self.padding_tensor_width, "CONSTANT")
+
+        conv_x = self.conv_layer_2(padded_x)
         conv_x = self.group_norm_layer_2(conv_x)
         conv_x = self.squeeze_excitation_layer_2(conv_x)
 
