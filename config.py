0ab2049fad7d7a227fbf0403f88d7af32d458944
gaetano-signorelli <gae797@yahoo.it>
2023-02-08 01:12:18 +0100

    Fixed MHA

--- config.py
+++ config.py
@@ -6,15 +6,15 @@ STANDARDIZE = True
 USE_LOG_SCALE = False
 TICKS_PER_BEAT = 960
 BPM = 60
-MAX_DURATION = 1468 #2048
-MAX_DELTA = 960 #1440
+MAX_DURATION = 2048 #1468
+MAX_DELTA = 2048 #960
 NOTES_LENGTH = 128 # define the number of notes a piece of music can have
 
 #Paths
 NOTES_CONVERSION_TABLE_PATH = os.path.join("data","notes_conversion_table.txt")
 DATAFRAME_PATH = os.path.join("data","dataframe.pkl")
 STATISTICS_PATH = os.path.join("data","statistics.pkl")
-DATASET_PATH = os.path.join("data","Free midi dataset")
+DATASET_PATH = os.path.join("data","Maestro dataset")
 WEIGHTS_PATH = "weights"
 UNET_WEIGHTS_PATH = os.path.join(WEIGHTS_PATH,"unet_{}.h5")
 
@@ -27,14 +27,14 @@ TIME_EMBEDDING_SIZE = 256
 
 #Training
 RUN_EAGERLY = True #False
-LEARNING_RATE = 1e-6
-EPOCHS = 40
-BATCH_SIZE = 64
+LEARNING_RATE = 1e-4
+EPOCHS = 100
+BATCH_SIZE = 128
 
 #Saving/Loading
 SAVE_MODEL = True
 LOAD_MODEL = True
-EPOCHS_BEFORE_SAVE = 5
+EPOCHS_BEFORE_SAVE = 20
 
 #Sampling
 N_SAMPLES = 2
